e32ff7bccdd550b3418d2bffba897d4c4ccd1d4d
Sam Ziegler <sziegler@siftscience.com>
2019-07-06 20:11:45 -0700

    added push pop

--- nand2tetris_ch7/src/com/nand2tetris/ch7/CodeWriter.java
+++ nand2tetris_ch7/src/com/nand2tetris/ch7/CodeWriter.java
@@ -32,6 +32,31 @@ public class CodeWriter {
                 out.write("A=M");
                 out.newLine();
                 out.write("M=D+M");
+                out.newLine();
+                break;
+        }
+    }
+
+    public void writePushPop(String cmd, String segment, String id) throws IOException {
+        switch(cmd) {
+            case "push":
+                switch(segment) {
+                    case "constant":
+                        out.write("@" + id);
+                        out.newLine();
+                        out.write("D=A");
+                        out.newLine();
+                        out.write("@0");
+                        out.newLine();
+                        out.write("A=M");
+                        out.newLine();
+                        out.write("M=D");
+                        out.newLine();
+                        out.write("@0");
+                        out.newLine();
+                        out.write("M=M+1");
+                        out.newLine();
+                }
         }
     }
 
--- nand2tetris_ch7/src/com/nand2tetris/ch7/Main.java
+++ nand2tetris_ch7/src/com/nand2tetris/ch7/Main.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Parser parser = new Parser("/Users/sziegler/testParser.txt");
-        while (parser.hasMoreCommands()) {
-            parser.advance();
-            System.out.println(parser.getCurrentCmd());
+        if (args.length < 1) {
+            System.out.println("oops!");
+            return;
         }
+        System.out.println(args[0]);
     }
 }
